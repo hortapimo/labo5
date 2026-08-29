674251d8f45da4363fa5f592bab36f245a8725b4
juan-ance <juampidel1@gmail.com>
2026-08-29 20:50:53 -0300

    deje todo como estaba antes

--- clase3/codigolabo5Clase3/grafico_ruido.py
+++ clase3/codigolabo5Clase3/grafico_ruido.py
@@ -1,15 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from pathlib import Path
+
 plt.style.use('./estiloGraficos.mplstyle')
 
 
-def tuki(archivo: Path):
-    data = np.genfromtxt(archivo, skip_header=53, skip_footer=1, usecols=(0,1), delimiter=',')
-    return data.T
 
-#%%
 def tuki2(archivo):
     df = pd.read_csv(archivo, skiprows=53, sep="\t")
     df.columns = ['l', 'I',"d"]
@@ -20,14 +16,18 @@ def tuki2(archivo):
     ampl = df["I"]
     fase = df["d"] 
     return v, ampl, fase
+#%%
+
+
+
+
 v, ampl, fase = tuki2("barrido_voltajes_-5_7_420nm_intento2_clase2.txt")
 
-fig, ax = plt.subplots()
-ax.errorbar(v,ampl, fmt=".")
 
-ax.set_xlabel("Voltaje [V]")
-ax.set_ylabel("Corriente [A]")
-plt.show()
+
+
+
+
 
 
 
