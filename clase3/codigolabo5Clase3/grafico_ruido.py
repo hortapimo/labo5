369ce78ddd0451b1fb26943f9c4754aa0c7ef4c9
juan-ance <juampidel1@gmail.com>
2026-08-29 21:30:58 -0300

    hice gráficos del ruido de las mediciones

--- clase3/codigolabo5Clase3/grafico_ruido.py
+++ clase3/codigolabo5Clase3/grafico_ruido.py
@@ -4,32 +4,30 @@ import pandas as pd
 
 plt.style.use('./estiloGraficos.mplstyle')
 
-
-
 def tuki2(archivo):
-    df = pd.read_csv(archivo, skiprows=53, sep="\t")
-    df.columns = ['l', 'I',"d"]
+    df = pd.read_csv(archivo, skiprows=1, sep=",")
+    df.columns = ['l', 'I',"d", "k"]
     df['l'] = pd.to_numeric(df['l'], errors='coerce')
     df['I'] = pd.to_numeric(df['I'], errors='coerce')
     df['d'] = pd.to_numeric(df['d'], errors='coerce')
+    df['k'] = pd.to_numeric(df['k'], errors='coerce')
     v = df["l"]
     ampl = df["I"]
-    fase = df["d"] 
-    return v, ampl, fase
+    fase = df["d"]
+    radio = df["k"]
+    return v, ampl, fase, radio
 #%%
+v, i, _, _= tuki2("medicionRuidoBlanco.txt")
 
+fig, ax = plt.subplots(figsize=(8,6))
 
-
-
-v, ampl, fase = tuki2("barrido_voltajes_-5_7_420nm_intento2_clase2.txt")
-
-
-
-
-
-
-
-
+ax.errorbar(v,i*1e12,yerr=np.abs(i)*0.01*1e12,fmt=".", label="Datos")
+ax.set_xlabel("Voltaje [V]")
+ax.set_ylabel("Corriente [pA]")
+ax.legend()
+fig.tight_layout()
+fig.savefig("RuidoBlanco.png")
+fig.show()
 
 
 
